fd208684c5f439e65952e352fb29259fb6fd5514
erica <2061784@alu.murciaeduca.es>
2024-03-10 15:51:02 +0100

    atributos encapsulados

--- src/main/java/org/example/Contacto.java
+++ src/main/java/org/example/Contacto.java
@@ -15,9 +15,9 @@ class Contacto {
     private List<String> phones;
 
     public Contacto(String name, String phone) {
-        this.name = name;
-        this.phones = new ArrayList<>();
-        this.phones.add(phone);
+        this.setName(name);
+        this.setPhones(new ArrayList<>());
+        this.getPhones().add(phone);
     }
 
     public String getName() {
@@ -26,5 +26,13 @@ class Contacto {
 
     public List<String> getPhones() {
         return this.phones;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPhones(List<String> phones) {
+        this.phones = phones;
     }
 }
